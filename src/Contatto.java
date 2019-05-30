91986ea3bda9380c66c31aee11c8f31087a4d645
fbgian <fgchaka@gmail.com>
2019-05-30 12:19:04 +0200

    initial commit

---
+++ src/Contatto.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Contatto {
+    //uso un arraylist
+    //id e tel
+
+    String nome;
+    String cognome;
+    String telefono;
+    List<String> rub = new ArrayList<String>();
+
+    void aggiungiContatto(String nome, String cognome, String telefono) {}
+
+    Contatto(String nome, String cognome, String telefono) {
+
+    }
+    /*
+    void rimuovi/();
+
+    string cerca();
+
+    String toString
+    */
+}
+
